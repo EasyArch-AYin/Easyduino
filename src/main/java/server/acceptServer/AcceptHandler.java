1cd36da8de53cd7c44ac29efc8f36521768bc4f7
11578 <1157878136@qq.com>
2020-02-07 18:11:05 +0800

    Tray

--- src/main/java/server/acceptServer/AcceptHandler.java
+++ src/main/java/server/acceptServer/AcceptHandler.java
@@ -2,11 +2,20 @@ package server.acceptServer;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
 import io.netty.util.CharsetUtil;
+import io.netty.util.concurrent.GlobalEventExecutor;
 
 public class AcceptHandler extends ChannelInboundHandlerAdapter {
+
+    //定义一个channel组，管理所有的channel
+    //GlobalEventExecutor.INSTANCE是全局事件执行器，是一个单例，用来帮我们执行channelGroup
+    private static ChannelGroup channelGroup = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg){
         ByteBuf byteBuf = (ByteBuf) msg;
@@ -14,8 +23,22 @@ public class AcceptHandler extends ChannelInboundHandlerAdapter {
     }
 
     @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        Channel channel = ctx.channel();
+        channelGroup.writeAndFlush("客户端" + channel.remoteAddress() + "上线了");
+        channelGroup.add(channel);
+    }
+
+    @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
         ctx.writeAndFlush(Unpooled.copiedBuffer("你已经连接到了8088端口", CharsetUtil.UTF_8));
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        Channel channel = ctx.channel();
+        channelGroup.writeAndFlush("客户端" + channel.remoteAddress() + "离开了");
+        channelGroup.add(channel);
     }
 
     @Override
--- src/main/java/server/dataServer/DataServerHandler.java
+++ src/main/java/server/dataServer/DataServerHandler.java
@@ -10,6 +10,7 @@ import io.netty.util.CharsetUtil;
 import java.net.InetAddress;
 
 public class DataServerHandler extends SimpleChannelInboundHandler<DatagramPacket> {
+
     protected void channelRead0(ChannelHandlerContext ctx, DatagramPacket msg) throws Exception {
         //获取客户端发来的信息
         ByteBuf byteBuf = msg.content();
@@ -21,5 +22,6 @@ public class DataServerHandler extends SimpleChannelInboundHandler<DatagramPacke
         System.out.println("客户端信息：" + byteBuf.toString(CharsetUtil.UTF_8));
         //回复信息
         ctx.channel().writeAndFlush(new DatagramPacket(Unpooled.copiedBuffer("服务器接收到了你的消息",CharsetUtil.UTF_8),msg.sender()));
+
     }
 }
